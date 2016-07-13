8f7625f53390d239dab239ebf5570a62d2f017cd
Aral Roca Gomez <aral.roca@greenlightech.com>
2016-07-13 09:14:57 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 const electron = require('electron');
-const { app, BrowserWindow, globalShortcut } = electron;
+const { app, BrowserWindow } = electron;
 const args = process.argv;
 
 let mainWindow;
